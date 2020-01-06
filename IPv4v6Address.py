bd85a7cc0e08842f54759fa214efd8e8dcb887c1
HarishRajGowda <harishrajgowda@hotmail.com>
2020-01-06 14:25:38 +0530

    IPv4 and/or IPv6 Addresses

--- IPv4v6Address.py
+++ IPv4v6Address.py
@@ -2,14 +2,27 @@ import ipaddress
 IPv4_Subnet = '172.16.0.0/27'
 IPv6_Subnet = '2002::/126'
 
-''' IPv4 Loop '''
-print('IP4 hosts:')
+
+print('IP4 Subnet:')
 ipv4 = ipaddress.IPv4Network(IPv4_Subnet)
 
-print('Subnet:'.format(ipv4))
-print('Subnet mask:'.format(ipv4.netmask))
-print('No. Of Valid Hosts:'.format(ipv4.num_addresses))
+sub = str(ipv4)
+netmask = str(ipv4.netmask)
+num_addresses = str(ipv4.num_addresses)
+
+print('Subnet:'.format(sub))
+print('Subnet mask:'.format(netmask))
+print('No. Of Valid Hosts:'.format(num_addresses))
 
 ''' IPv6 Host '''
-print('IPv6 hosts:')
+print('IPv6 Subnet:')
 ipv6 = ipaddress.IPv6Network(IPv6_Subnet)
+
+sub = str(ipv6)
+netmask = str(ipv6.netmask)
+num_addresses = str(ipv6.num_addresses)
+
+print('Subnet:'.format(sub))
+print('Subnet mask:'.format(netmask))
+print('No. Of Valid Hosts:'.format(num_addresses))
+
